ef787e0e601f677a9c22b58b3e0c2e607b34dfb8
zkroliko <zbkrolikowski@gmail.com>
2016-10-11 17:44:13 +0200

    Initial

---
+++ build.sbt
@@ -0,0 +1,14 @@
+import _root_.sbt.Keys._
+
+name := "AkkaAuction"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.11",
+  "com.typesafe.akka" %% "akka-testkit" % "2.4.11" % "test",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test")
---
+++ src/main/scala-2.11/Auction.scala
@@ -0,0 +1,36 @@
+
+import akka.actor._
+import akka.dispatch.sysmsg.Create
+import akka.event.LoggingReceive
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+object Action {
+
+  case class Create(starting : BigDecimal) {}
+
+  case class AskPrice() {}
+
+  case class PriceInfo(current : BigDecimal) {}
+
+  case class Bid(proposed : BigDecimal) {}
+
+  case class BidAck(accepted : BigDecimal) {}
+
+  case class BidNack(price : BigDecimal){}
+
+}
+
+class Auction extends Actor {
+  import Action._
+
+  def receive = LoggingReceive {
+    case Create(starting) => context.become(created(starting))
+  }
+
+  def created(starting : BigDecimal): Receive = LoggingReceive {
+    case Bid(proposed) =>
+  }
+
+}
