529e7ad3f4d033a2754cb9f66105d64289d44b55
zkroliko <zbkrolikowski@gmail.com>
2016-10-15 21:17:14 +0200

    Added more auctions and bidders to main

--- src/main/scala-2.11/auctionHouse/AuctionHouse.scala
+++ src/main/scala-2.11/auctionHouse/AuctionHouse.scala
@@ -3,6 +3,7 @@ package auctionHouse
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import auctionHouse.Auction.Start
 import com.github.nscala_time.time.Imports._
 import scala.concurrent.duration.Duration
 
@@ -10,12 +11,17 @@ object AuctionHouse extends App {
 
   case class AuctionList(auctions: List[ActorRef])
 
+  val nAuctions = 10
+  val nBidders = 1
+
   val system = ActorSystem("auctionHouse")
   val auction = system.actorOf(Props(new Auction("a book",200.0)))
   val bidder = system.actorOf(Props[Bidder])
   val bidder2 = system.actorOf(Props[Bidder])
 
-  auction ! Auction.Start
-  bidder ! AuctionList(List(auction))
-  bidder2 ! AuctionList(List(auction))
+  val auctions = (1 to nAuctions).map(n =>system.actorOf(Props(new Auction("a book"+n,200.0+n)))).toList
+  val bidders = (1 to nBidders).map(n => system.actorOf(Props[Bidder])).toList
+
+  auctions.foreach(_ ! Start)
+  bidders.foreach(_ ! AuctionList(auctions))
 }
