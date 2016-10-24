78c8379fea4ecf1d0bed86ad0808caf999de0e86
zkroliko <zbkrolikowski@gmail.com>
2016-10-24 13:19:40 +0200

    Fix

--- src/main/scala-2.11/auctionHouse/AuctionHouse.scala
+++ src/main/scala-2.11/auctionHouse/AuctionHouse.scala
@@ -13,13 +13,13 @@ object AuctionHouse {
 class AuctionHouse extends Actor {
   import AuctionHouse._
 
-  val nSellers = 1
-  val nAuctionsPerSeller = 10
-  val nBidders = 1
+  val nSellers = 3
+  val nAuctionsPerSeller = 4
+  val nBidders = 5
 
   def receive = LoggingReceive {
     case Init =>
-      val sellers = (1 to nSellers).map{n => context.actorOf(Props[Seller],"seller1")}
+      val sellers = (1 to nSellers).map{n => context.actorOf(Props[Seller],"seller"+n)}
       val descriptions = (1 to nAuctionsPerSeller).map(n => AuctionDescription("item"+n.toString,200.0+n))
       sellers.foreach{s => s ! BuildFromDescriptions(descriptions.toList)}
       val bidders = (1 to nBidders).map(n => context.actorOf(Props[Bidder],s"bidder$n")).toList
